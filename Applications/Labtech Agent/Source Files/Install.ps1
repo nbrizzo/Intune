e69dab292f6e4389c65fa6ed6905a2666af2d8a4
nbrizzo <92049913+nbrizzo@users.noreply.github.com>
2022-11-27 20:18:28 -0600

    Add files via upload

---
+++ Applications/Labtech Agent/Source Files/Install.ps1
@@ -0,0 +1,6 @@
+#Labtech Install
+
+#Call installer from Intune app package
+
+msiexec /i "Install.msi" /quiet /norestart SERVERADDRESS=<CWA Server URL> SERVERPASS=
+<Server Password> LOCATION=<location ID>
