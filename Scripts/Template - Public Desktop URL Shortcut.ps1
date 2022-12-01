beda1f496f2168347cb780305f9777fab06ef625
nbrizzo <92049913+nbrizzo@users.noreply.github.com>
2022-12-01 10:47:18 -0600

    Add files via upload

---
+++ Scripts/Template - Public Desktop URL Shortcut.ps1
@@ -0,0 +1,17 @@
+﻿<#
+Created by Nick Brizzolara
+
+.SYNOPSIS
+Create a Public Desktop URL Shortcut
+
+#>
+$WebsiteURL = "<URL of Website>"
+$ShortcutName = "CHANGE ME.url"
+$wshShell = New-Object -ComObject "WScript.Shell"
+
+$urlShortcut = $wshShell.CreateShortcut(
+  (Join-Path $wshShell.SpecialFolders.Item("AllUsersDesktop") $ShortcutName)
+)
+
+$urlShortcut.TargetPath = $WebsiteURL
+$urlShortcut.Save()
