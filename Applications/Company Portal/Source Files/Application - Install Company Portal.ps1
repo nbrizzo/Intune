2eb17b2be185e1da91b43694b0ecd4ee6f27f07d
nbrizzo <92049913+nbrizzo@users.noreply.github.com>
2022-12-01 10:26:37 -0600

    Add files via upload

---
+++ Applications/Company Portal/Source Files/Application - Install Company Portal.ps1
@@ -0,0 +1,10 @@
+<#
+.SYNOPSIS
+Created by Nick Brizzolara
+Created on 12/1/2022
+
+Script uses Winget to install the company portal
+
+#>
+
+Winget Install 9WZDNCRFJ3PZ --accept-source-agreements --accept-package-agreements
---
+++ Applications/Company Portal/Source Files/Application - Uninstall Company Portal.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+Created by Nick Brizzolara
+Created on 12/1/2022
+
+Script uses Winget to uninstall the company portal
+
+#>
+
+Winget Uninstall 9WZDNCRFJ3PZ
+
+
