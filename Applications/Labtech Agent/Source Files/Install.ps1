acbf19d8880847ce48d12400559f5836653c544f
nbrizzo <92049913+nbrizzo@users.noreply.github.com>
2022-12-01 10:45:25 -0600

    Update Install.ps1

--- Applications/Labtech Agent/Source Files/Install.ps1
+++ Applications/Labtech Agent/Source Files/Install.ps1
@@ -1,5 +1,26 @@
-#Labtech Install
+<#
+Created by Nick Brizzolara
 
-#Call installer from Intune app package
+.SYNOPSIS 
+Intune W32 Application Install Template.
 
-msiexec /i "Install.msi" /quiet /norestart SERVERADDRESS=<CWA Server URL> SERVERPASS=<Server Password> LOCATION=<location ID>
+.Description
+Script checks if temp directory exists and creates if it does not. Script then installs package silently with verbose logging enabled.
+
+Rename Script to Install.ps1 when finished editing variables.
+#>
+$TempDirectory = "C:\Windows\Temp"
+$TempExists = Test-Path $TempDirectory 
+$Installer = "Install.msi"
+
+If ($TempExists -eq $false) {
+
+    New-Item $TempDirectory -ItemType Directory
+
+    elseif ($TempExists -eq $true) {
+        Write-Host "Temp Directory Already Exists"
+    }{
+    }
+}
+
+msiexec /i $Installer /qn /L*v "C:\Temp\LogName.Log" SERVERADDRESS=<URL> SERVERPASS=<Password> LOCATION=<LocationID>
